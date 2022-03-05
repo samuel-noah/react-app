d72b91c917030e5ca117ad0f3c07d953bd03f125
Samuel William Noah <noah.samuelwilliam@gmail.com>
2022-03-06 00:06:36 +0700

    Adding the home section

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 const Home = () => {
     return (  
         <div className="home">
-            <h1>Hi I am Samuel </h1>
+            <h5>Hello,</h5>
+            <h1>My Name is Samuel </h1>
             <p> I am an active student Manjoring Creative Business 
                 in the University of Indonesia. I am interested in
                 Tech Develompment especially in Software Development.
