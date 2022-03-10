dffa05f45ba6b56bdd5b340eb8656be6a168fb8e
Samuel William Noah <noah.samuelwilliam@gmail.com>
2022-03-10 21:22:01 +0700

    Adding useState

---
+++ src/Projectlist.js
@@ -0,0 +1,20 @@
+const Projectlist = () => {
+    return (
+        <div className="projects-container">
+            <div className="projectlist">
+                <h3>CoWork</h3>
+                <h5>A CRUD web app using PHP and MySqli DBMS</h5>
+            </div>
+            <div className="projectlist">
+                <h3>samuelwilliam.com</h3>
+                <h5>A personal portofolio website with React</h5>
+            </div>
+            <div className="projectlist">
+                <h3>OPEN AI</h3>
+                <h5>A simple chatbot using node js and Open AI API</h5>
+            </div>
+        </div>
+     );
+}
+ 
+export default Projectlist;
--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
+import Projectlist from "./Projectlist";
+
 const Projects = () => {
+    const [projects, setProjects] = useState('');
+
+    const handleClick = () => {
+        setProjects(
+        <Projectlist>
+
+        </Projectlist>)
+    }
+
     return ( 
         <section className="projects">
             <h1>Projects &#128079;</h1>
             <p> I love Idea Orieanted task that and problem solving activity.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Curabitur euismod, nisi vel consectetur euismod,
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Curabitur euismod, nisi vel consectetur euismod,
             </p>
                 <div className="projects-content">
-                    <ul>
-                        <li> React Js </li>
-                        <li> Express JS Framework  </li>
-                        <li> Mongo DB </li>
-                    </ul>
                 </div>
+                <button onClick={handleClick}>See Projects</button>
+                <p>{projects}</p>
         </section>
      );
 }
